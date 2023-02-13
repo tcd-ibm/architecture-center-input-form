1cafae0453f44248061fbae032c8e32e9f7d78e9
Jacky Su <hxsushi@126.com>
2023-02-13 23:12:24 +0000

    implement filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,113 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import React from 'react'
 import './App.scss'
 import SearchBar from './Components/SearchBar'
 import AsciidocEditor from './Components/AsciidocEditor'
 import Card from './Components/Card'
-import { Button, Tag } from '@carbon/react'
+import { Button, Tag, Accordion, AccordionItem, Checkbox } from '@carbon/react'
+import axios from 'axios'
+
+export const FilterContext = React.createContext()
 
 function App() {
 
   const [isEditorOn, SetIsEditorOn] = useState(false)
+  const [filterTagList, setFilterTagList] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const [productTags, setProductTags] = useState([])
+  const getProductTags = async () => {
+    const res = await axios.get('product')
+    setProductTags(res.data)
+  }
+
+  const [solutionTags, setSolutionTags] = useState([])
+  const getSolutionTags = async () => {
+    const res = await axios.get('solution')
+    setSolutionTags(res.data)
+  }
+
+  const [verticalTags, setVerticalTags] = useState([])
+  const getVerticalTags = async () => {
+    const res = await axios.get('vertical')
+    setVerticalTags(res.data)
+  }
+
+  const [typeTags, setTypeTags] = useState([])
+  const getTypeTags = async () => {
+    const res = await axios.get('type')
+    setTypeTags(res.data)
+  }
+
+  const handlerFilterChange = (event) => {
+    const { checked, id } = event.target
+    if (checked) {
+      console.log(filterTagList)
+      setFilterTagList([...filterTagList, id])
+    } else {
+      setFilterTagList(filterTagList.filter((item) => item !== id))
+    }
+
+  }
+  useEffect(() => {
+    Promise.all([getProductTags(), getSolutionTags(), getVerticalTags(), getTypeTags()]).then(() => {
+      setIsLoading(false)
+    })
+  }, [])
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
 
   const editor = isEditorOn ? <AsciidocEditor /> : null
+
   return (
     <>
+      <FilterContext.Provider value={{ filterTagList, setFilterTagList }}>
+        <div id="bodyContainer">
+          {editor}
+          <div id="sidebarContainer">
+            <div class="fixed_elements">
+              <SearchBar />
+              <Button onClick={() => SetIsEditorOn(() => !isEditorOn)}>Editor</Button>
+              <Accordion>
 
-      
-      <div id="bodyContainer">
-        {editor}
-        <div id="sidebarContainer">
-          <div class="fixed_elements">
-            <SearchBar />
-            <Button onClick={() => SetIsEditorOn(() => !isEditorOn)}>Editor</Button>
+                <AccordionItem title="Solution">
+                  <fieldset className="cds--fieldset">
+                    {solutionTags.map((item, _) => <Checkbox labelText={item.sname} id={item.sid} checked={filterTagList.includes(item.sid)} onChange={handlerFilterChange} />)}
+                  </fieldset>
+                </AccordionItem>
+
+                <AccordionItem title="Vertical">
+                  <fieldset className="cds--fieldset">
+                    {verticalTags.map((item, _) => <Checkbox labelText={item.vname} id={item.vid} checked={filterTagList.includes(item.vid)} onChange={handlerFilterChange} />)}
+                  </fieldset>
+                </AccordionItem>
+
+                <AccordionItem title="Product">
+                  <fieldset className="cds--fieldset">
+                    {productTags.map((item, _) => <Checkbox labelText={item.pname} id={item.pid} checked={filterTagList.includes(item.pid)} onChange={handlerFilterChange} />)}
+                  </fieldset>
+                </AccordionItem>
+
+                <AccordionItem title="Type">
+                  <fieldset className="cds--fieldset">
+                    {typeTags.map((item, _) => <Checkbox labelText={item.typename} id={item.tid} checked={filterTagList.includes(item.tid)} onChange={handlerFilterChange} />)}
+                  </fieldset>
+                </AccordionItem>
+
+              </Accordion>
+            </div>
           </div>
+
+          <div /* separator */ />
+
+          <div id="cardContainer">
+            <Card />
+          </div>
+
         </div>
-
-        <div /* separator */ />
-
-        <div id="cardContainer">
-          <Card />
-        </div>
-
-      </div>
+      </FilterContext.Provider>
     </>
   )
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,15 +1,19 @@
 //import { Button } from '@carbon/react'
 import axios from 'axios'
 import './card.scss'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { Tag } from '@carbon/react'
 
 const CARD_IMG_URL = "http://www.redhat.com/architect/portfolio/repo/images/"
 
+import { FilterContext } from '@/App.jsx'
+
 export default function Card() {
 
+  const { filterTagList } = useContext(FilterContext)
   const [isLoading, setIsLoading] = useState(true)
   const [result, setResult] = useState([])
+  const [filteredResult, setFilteredResult] = useState([])
 
   useEffect(() => {
     axios.get('pa/0').then((res) => {
@@ -21,42 +25,58 @@ export default function Card() {
       })
   }, [])
 
+  useEffect(() => {
+    if (filterTagList.length > 0) {
+      console.log(filterTagList)
+      var results = []
+      for (const tag of filterTagList) {
+        for (const item of result) {
+          if (item.ProductType.includes(tag) || item.Solutions.includes(tag) || item.Vertical.includes(tag) || item.Product.includes(tag)) {
+            results.push(item)
+          }
+        }
+      }
+      setFilteredResult(results)
+    }
+  }, [filterTagList])
+
   if (isLoading) {
     return <div>Loading...</div>
   }
 
   return (
     <>
-      {result.map((item, _) =>
-      (
-        item.islive ?
-          <div className='card'>
-            <img src={CARD_IMG_URL + item.Image1Url} className='cardImage' />
-            <h4 className='cardHeader'>{item.Heading}</h4>
-            <div className='cardContent'>{item.Summary}</div>
-
-            {
-              item.ProductType.split(",").map(typetag =>
-                <Tag type="red" title="Clear Filter" key={item.ppid + typetag}></Tag>
-              )}
-            {
-              item.Solutions.split(",").map(solutiontag =>
-                <Tag type="magenta" title="Clear Filter" key={item.ppid + solutiontag}>{solutiontag}</Tag>
-              )}
-            {
-              item.Vertical.split(",").map(verticaltag =>
-                <Tag type="warm-gray" title="Clear Filter" key={item.ppid + verticaltag} >{verticaltag}</Tag>
-              )}
-            {
-              item.Product.split(",").map(producttag =>
-                <Tag type="cool-gray" title="Clear Filter" key={item.ppid + producttag}>{producttag}</Tag>
-              )}
-
-          </div>
-          : null
-      )
-      )
+      {
+        filterTagList.length > 0 ? cardList({ list: filteredResult }) : cardList({ list: result })
       }
     </>
   )
+}
+
+function cardList({ list }) {
+  return (list.map((item, _) =>
+    <div className='card'>
+      <img src={CARD_IMG_URL + item.Image1Url} className='cardImage' />
+      <h4 className='cardHeader'>{item.Heading}</h4>
+      <div className='cardContent'>{item.Summary}</div>
+
+      {
+        item.ProductType.split(",").map(typetag =>
+          <Tag type="red" title="Clear Filter" key={item.ppid + typetag}></Tag>
+        )}
+      {
+        item.Solutions.split(",").map(solutiontag =>
+          <Tag type="magenta" title="Clear Filter" key={item.ppid + solutiontag}>{solutiontag}</Tag>
+        )}
+      {
+        item.Vertical.split(",").map(verticaltag =>
+          <Tag type="warm-gray" title="Clear Filter" key={item.ppid + verticaltag} >{verticaltag}</Tag>
+        )}
+      {
+        item.Product.split(",").map(producttag =>
+          <Tag type="cool-gray" title="Clear Filter" key={item.ppid + producttag}>{producttag}</Tag>
+        )}
+
+    </div>
+  ))
 }
