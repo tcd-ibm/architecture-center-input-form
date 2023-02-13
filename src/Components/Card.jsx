cab7a244ea42e1eebcecdca260ee9962b2aa56b1
Michał Bronicki <bronickm@tcd.ie>
2023-02-13 23:40:00 +0000

    Changed Card component styling

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,35 +1,31 @@
-import { Tag } from '@carbon/react';
+import { Tile, Link, Tag } from '@carbon/react';
 import './card.scss';
 
 const CARD_IMG_URL = "http://www.redhat.com/architect/portfolio/repo/images/";
 
-export default function Card({projectData}) {
+export default function CardNew({projectData}) {
 
   if(!projectData.islive) return null;
   
   return (
-    <div className='card'>
-      <img src={CARD_IMG_URL + projectData.Image1Url} className='cardImage' />
-      <h4 className='cardHeader'>{projectData.Heading}</h4>
-      <div className='cardContent'>{projectData.Summary}</div>
-
-      {
-        projectData.ProductType.split(",").map(typetag =>
-          <Tag type="red" title="Clear Filter" key={projectData.ppid + typetag}></Tag>
+    <Tile className='tile'>
+      {/* <img src={CARD_IMG_URL + projectData.Image1Url} className='cardImage' /> */}
+      <Link size='lg' className='titleLink'>{projectData.Heading}</Link>
+      <p>{projectData.Summary}</p>
+      <div className='tags'>
+        { projectData.ProductType.split(",").map(typetag =>
+            <Tag type="red" title="Clear Filter" key={projectData.ppid + typetag}></Tag>
         )}
-      {
-        projectData.Solutions.split(",").map(solutiontag =>
-          <Tag type="magenta" title="Clear Filter" key={projectData.ppid + solutiontag}>{solutiontag}</Tag>
+        { projectData.Solutions.split(",").map(solutiontag =>
+            <Tag type="magenta" title="Clear Filter" key={projectData.ppid + solutiontag}>{solutiontag}</Tag>
         )}
-      {
-        projectData.Vertical.split(",").map(verticaltag =>
-          <Tag type="warm-gray" title="Clear Filter" key={projectData.ppid + verticaltag} >{verticaltag}</Tag>
+        { projectData.Vertical.split(",").map(verticaltag =>
+            <Tag type="warm-gray" title="Clear Filter" key={projectData.ppid + verticaltag} >{verticaltag}</Tag>
         )}
-      {
-        projectData.Product.split(",").map(producttag =>
-          <Tag type="cool-gray" title="Clear Filter" key={projectData.ppid + producttag}>{producttag}</Tag>
+        { projectData.Product.split(",").map(producttag =>
+            <Tag type="cool-gray" title="Clear Filter" key={projectData.ppid + producttag}>{producttag}</Tag>
         )}
-
-    </div>
+      </div>
+    </Tile>
   );
 }
