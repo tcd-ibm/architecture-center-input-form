35db587936dfebb27efacd32975ac3932ab10507
Michał Bronicki <bronickm@tcd.ie>
2023-02-18 17:58:33 +0000

    Implemented login form components

---
+++ src/Components/LoginFormGeneric.jsx
@@ -0,0 +1,56 @@
+import { forwardRef } from 'react';
+import { Tile, FluidForm, InlineNotification, Checkbox, Button } from '@carbon/react';
+import { ArrowRight } from '@carbon/icons-react';
+import './LoginFormGeneric.scss';
+
+import EmailInput from './EmailInput';
+import PasswordInput from './PasswordInput';
+
+function LoginFormGeneric(props, ref) {
+    const {subheadingContentNode, errorText, inputType, rememberIdCheckbox, buttonText, onSubmit} = props;
+
+    return (
+        <Tile className='loginFormTile'>
+            <FluidForm>
+                <div className='innerContainer'>
+                    <p className='heading'>Log in</p>
+                    <p>{subheadingContentNode}</p>
+                    
+                    { errorText ?
+                        <InlineNotification
+                            title='Error:'
+                            subtitle={errorText}
+                            lowContrast={true}
+                            className='notification'
+                        />
+                        :
+                        <div className='notificationPlaceholder'></div>
+                    }
+                    
+                    { inputType === 'email' &&
+                        <EmailInput className='input' ref={ref} />
+                    }
+                    { inputType === 'password' &&
+                        <PasswordInput className='input' ref={ref} />
+                    }
+
+                    { rememberIdCheckbox ?
+                        <div><Checkbox labelText='Remember ID' id='remember-id-checkbox' className='checkbox' /></div> :
+                        <div style={{visibility: 'hidden'}}><Checkbox id='remember-id-checkbox' className='checkbox' /></div>
+                    }
+                    
+                </div>
+                <div className='buttonContainer'>
+                    <div className='flexColumn'></div>
+                    <div className='flexColumn'>
+                        <Button renderIcon={ArrowRight} className='button' onClick={onSubmit}>{buttonText}</Button>
+                    </div>
+                </div>
+            </FluidForm>
+        </Tile>
+    );
+}
+
+LoginFormGeneric = forwardRef(LoginFormGeneric);
+
+export default LoginFormGeneric;
