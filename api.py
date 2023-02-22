1bc7f05928cc2ac583e990a5053c50e5a71308c2
Jacky Su <hxsushi@126.com>
2023-02-22 22:42:02 +0000

    implement basic query limit and offset on getting project list

--- api.py
+++ api.py
@@ -27,6 +27,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 SALT = "fa3e1b071f78d55d833c2df51a3089e5"
 
+MAX_PAGE_SIZE = 50
 # templates = Jinja2Templates(directory="templates")
 
 # 这里是 tokenUrl，而不是 token_url，是为了和 OAuth2 规范统一
@@ -75,7 +76,8 @@ def _decode_token(token: str) -> dict:
         raise unauthorized_error("Could not validate credentials")
 
 
-async def get_all_users_db(session: AsyncSession = Depends(get_session)) -> List[User]:
+async def get_all_users_db(session: AsyncSession = Depends(
+    get_session)) -> List[User]:
     result = await session.execute(select(User))
     users = result.scalars().all()
     return [User(**user.__dict__) for user in users]
@@ -118,7 +120,8 @@ async def on_startup():
 @router.post("/user/signup")
 async def add_user(user: UserSignup,
                    session: AsyncSession = Depends(get_session)):
-    if not user.email or not re.match(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", user.email):
+    if not user.email or not re.match(
+            r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", user.email):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="Email invalid")
 
@@ -128,19 +131,23 @@ async def add_user(user: UserSignup,
                             detail="Email registered already")
 
     if not user.password or len(user.password) < 8:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="Password invalid, should be at least 8 characters")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Password invalid, should be at least 8 characters")
 
     new_user = User(email=user.email,
-                     hashed_password=get_password_hash(user.password + SALT),
-                     id=str(uuid4()))
+                    hashed_password=get_password_hash(user.password + SALT),
+                    id=str(uuid4()))
 
     session.add(new_user)
     await session.commit()
     await session.refresh(new_user)
 
     token = _create_token(
-        data={"sub": user.username, "role": 0},
+        data={
+            "sub": user.username,
+            "role": 0
+        },
         expires=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     response = {"access_token": token, "token_type": "bearer"}
     return response
@@ -207,7 +214,10 @@ async def login(form: OAuth2PasswordRequestForm = Depends(),
         raise unauthorized_error("Incorrect username or password")
 
     token = _create_token(
-        data={"sub": form.username, "role": user.role},
+        data={
+            "sub": form.username,
+            "role": user.role
+        },
         expires=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     response = {"access_token": token, "token_type": "bearer"}
     return response
@@ -222,7 +232,9 @@ async def get_private_endpoint(current_user: User = Depends(get_current_user)):
 
 
 @router.get("/user/private/admin")
-async def get_private_admin_endpoint(current_user: User = Depends(get_current_user), session: AsyncSession = Depends(get_session)):
+async def get_private_admin_endpoint(
+        current_user: User = Depends(get_current_user),
+        session: AsyncSession = Depends(get_session)):
     if not current_user.role:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail="Unauthorized")
@@ -254,7 +266,10 @@ async def add_project(project: ProjectBase,
         tags.append(tag)
 
     data["tags"] = tags
-    new_project = Project(**data, id=str(uuid4()), user=current_user, email=current_user.email)
+    new_project = Project(**data,
+                          id=str(uuid4()),
+                          user=current_user,
+                          email=current_user.email)
 
     session.add(new_project)
     await session.commit()
@@ -263,13 +278,38 @@ async def add_project(project: ProjectBase,
 
 
 @router.get("/user/projects", response_model=List[ProjectWithUserAndTags])
-async def get_projects(session: AsyncSession = Depends(get_session),
-                       current_user: User = Depends(get_current_user)) -> List[ProjectWithUserAndTags]:
+async def get_user_projects(
+    per_page: int = 30,
+    page: int = 1,
+    session: AsyncSession = Depends(get_session),
+    current_user: User = Depends(get_current_user)
+) -> List[ProjectWithUserAndTags]:
     if not current_user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail="Unauthorized")
 
-    r = await session.execute(select(Project).options(selectinload(Project.user), selectinload(Project.tags)).order_by(Project.id))
+    r = await session.execute(
+        select(Project).where(Project.email == current_user.email).options(selectinload(Project.user),
+                                selectinload(Project.tags)).order_by(
+                                    Project.id).offset(
+                                        max((page - 1) * per_page,
+                                        0)).limit(min(per_page, MAX_PAGE_SIZE)))
+    return r.scalars().all()
+
+
+@router.get("/projects", response_model=List[ProjectWithUserAndTags])
+async def get_all_projects(
+    per_page: int = 30,
+    page: int = 1,
+    session: AsyncSession = Depends(get_session),
+) -> List[ProjectWithUserAndTags]:
+
+    r = await session.execute(
+        select(Project).options(selectinload(Project.user),
+                                selectinload(Project.tags)).order_by(
+                                    Project.id).offset(
+                                        max((page - 1) * per_page,
+                                        0)).limit(min(per_page, MAX_PAGE_SIZE)))
     return r.scalars().all()
 
 
@@ -280,8 +320,8 @@ async def fetch_announcement(request: Request,
     r = await session.execute(
         select(Announcement).filter_by(**request.query_params._dict, aid=aid)
     ) if aid else await session.execute(
-        select(Announcement).filter_by(**request.query_params._dict).order_by(
-            Announcement.aid))
+        select(Announcement).filter_by(
+            **request.query_params._dict).order_by(Announcement.aid))
     return r.scalars().all()
 
 
@@ -292,8 +332,8 @@ async def fetch_detail(request: Request,
     r = await session.execute(
         select(Detail).filter_by(**request.query_params._dict, ppid=ppid)
     ) if ppid else await session.execute(
-        select(Detail).filter_by(**request.query_params._dict).order_by(
-            Detail.ppid))
+        select(Detail).filter_by(
+            **request.query_params._dict).order_by(Detail.ppid))
     return r.scalars().all()
 
 
@@ -312,19 +352,25 @@ async def fetch_pa(request: Request,
 async def fetch_tag(request: Request,
                     session: AsyncSession = Depends(get_session),
                     tagId: int = 0) -> List[Tag]:
-    r = await session.execute(select(Tag).filter_by(**request.query_params._dict, tagId=tagId))
+    r = await session.execute(
+        select(Tag).filter_by(**request.query_params._dict, tagId=tagId))
     return r.scalars().all()
 
 
 @router.get("/tags", response_model=List[CategoryWithTags])
-async def fetch_tags(session: AsyncSession = Depends(get_session)) -> List[CategoryWithTags]:
+async def fetch_tags(session: AsyncSession = Depends(
+    get_session)) -> List[CategoryWithTags]:
 
-    r = await session.execute(select(Category).options(selectinload(Category.tags)).order_by(Category.categoryId))
+    r = await session.execute(
+        select(Category).options(selectinload(Category.tags)).order_by(
+            Category.categoryId))
     return r.scalars().all()
 
 
 @router.get("/product", response_model=List[Product])
-async def fetch_product(request: Request, session: AsyncSession = Depends(get_session)) -> List[Product]:
+async def fetch_product(
+    request: Request, session: AsyncSession = Depends(get_session)
+) -> List[Product]:
     r = await session.execute(
         select(Product).filter_by(**request.query_params._dict).order_by(
             Product.pid))
@@ -332,7 +378,9 @@ async def fetch_product(request: Request, session: AsyncSession = Depends(get_se
 
 
 @router.get("/solution", response_model=List[Solution])
-async def fetch_solution(request: Request, session: AsyncSession = Depends(get_session)) -> List[Solution]:
+async def fetch_solution(
+    request: Request, session: AsyncSession = Depends(get_session)
+) -> List[Solution]:
     r = await session.execute(
         select(Solution).filter_by(**request.query_params._dict).order_by(
             Solution.sid))
@@ -340,14 +388,19 @@ async def fetch_solution(request: Request, session: AsyncSession = Depends(get_s
 
 
 @router.get("/type", response_model=List[Type])
-async def fetch_type(request: Request, session: AsyncSession = Depends(get_session)) -> List[Type]:
+async def fetch_type(
+    request: Request,
+    session: AsyncSession = Depends(get_session)) -> List[Type]:
     r = await session.execute(
-        select(Type).filter_by(**request.query_params._dict).order_by(Type.tid))
+        select(Type).filter_by(**request.query_params._dict).order_by(Type.tid)
+    )
     return r.scalars().all()
 
 
 @router.get("/vertical", response_model=List[Vertical])
-async def fetch_vertical(request: Request, session: AsyncSession = Depends(get_session)) -> List[Vertical]:
+async def fetch_vertical(
+    request: Request, session: AsyncSession = Depends(get_session)
+) -> List[Vertical]:
     r = await session.execute(
         select(Vertical).filter_by(**request.query_params._dict).order_by(
             Vertical.vid))
