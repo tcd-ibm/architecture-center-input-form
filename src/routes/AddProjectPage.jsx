b4e1c2da32f71dadca4b20fb6abb5362529cefd0
Tom Moroney <moroneyt@tcd.ie>
2023-02-23 02:19:15 +0000

    Updated "AddProject" page with submission form

--- src/routes/AddProjectPage.jsx
+++ src/routes/AddProjectPage.jsx
@@ -1,4 +1,4 @@
-import { Content } from '@carbon/react';
+import { Tile, Content, Tag, FormGroup, Stack, TextInput, Button } from '@carbon/react';
 
 import MainHeader from '@/Components/MainHeader';
 import DocEditor from '@/Components/AsciidocEditor';
@@ -7,8 +7,19 @@ function AddProjectPage() {
     return (
         <>
         <MainHeader />
-        <Content>
-            <DocEditor />
+        <Content style={{marginLeft: '20px', marginRight: '20px'}}>
+            <h1 style={{marginBottom: '10px'}}>Add New Project</h1>
+            <FormGroup>
+                <Stack gap={5}>
+                    <TextInput id="one" labelText="Project Title" />
+                    <TextInput id="two" labelText="Link to Project" />
+                    <Tile>
+                        <h4 style={{marginBottom: '10px'}}>Main Content</h4>
+                        <DocEditor />
+                    </Tile>
+                    <Button>Submit</Button>
+                </Stack>
+            </FormGroup>
         </Content>
         </>
     );
