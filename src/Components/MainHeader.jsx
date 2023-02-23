42cd34679af2fc811c4cfa41a681eeb6dcb07e4a
James Gray <grayj1@tcd.ie>
2023-02-23 16:28:16 +0000

    Improved header UI.

--- src/Components/MainHeader.jsx
+++ src/Components/MainHeader.jsx
@@ -1,18 +1,28 @@
-import { Header, HeaderName, HeaderNavigation, HeaderMenuItem } from '@carbon/react';
+import { Header, HeaderName, SkipToContent } from '@carbon/react';
+import { HeaderGlobalBar, HeaderGlobalAction } from '@carbon/react';
+import { Add, UserAvatar, Star, Bee } from '@carbon/icons-react';
 
 //TODO replace standard hrefs with react-router Link component or equivalent
 
 function MainHeader() {
     return (
         <Header aria-label="Amazing SwEng Project">
-            <HeaderName href="/" prefix="">
-                Amazing SwEng Project
+            <HeaderName href="/" prefix="Project Showcase:">
+                SHOWCASE NAME
             </HeaderName>
-            <HeaderNavigation aria-label="Amazing SwEng Project">
-                <HeaderMenuItem href="/add">
-                    Add new project
-                </HeaderMenuItem>
-            </HeaderNavigation>
+            <HeaderGlobalBar aria-label="Amazing SwEng Project">
+                <HeaderGlobalAction href="/" aria-label="Favourites">
+                    <Star size={20}/>
+                </HeaderGlobalAction>
+                <HeaderGlobalAction href="/add" aria-label="Add Project">
+                    <Add size={20}/>
+                </HeaderGlobalAction>
+                <HeaderGlobalAction href="/" aria-label="My Profile"
+                    tooltipAlignment="end">
+                    <UserAvatar size={20}/>
+                </HeaderGlobalAction>
+            </HeaderGlobalBar>
+            
         </Header>
     );
 }
