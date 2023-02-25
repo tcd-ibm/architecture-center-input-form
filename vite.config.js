93a1e950e2a69311916c01dcbb147872c00d3efa
Michał Bronicki <bronickm@tcd.ie>
2023-02-25 13:21:28 +0000

    Added eslint

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
