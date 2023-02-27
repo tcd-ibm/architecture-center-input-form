a5e87847361dedeeac69142c7fa399072df59486
Michał Bronicki <bronickm@tcd.ie>
2023-02-27 21:42:13 +0000

    ErrorPage global css bugfix

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,13 +1,18 @@
 import { Content } from '@carbon/react';
 import MainHeader from '@/Components/MainHeader';
-import PageNotFound from '@/Components/PageNotFound';
+
+import styles from './ErrorPage.module.scss';
 
 function ErrorPage() {
     return (
         <>
         <MainHeader />
-        <Content>
-            <PageNotFound />
+        <Content className={styles.mainContainer}>
+            <h1>404 Page not found!</h1>
+            <br></br><br></br><br></br>
+            <h4>Unfortunately, the page you are looking for does not exist.</h4>
+            <br></br>
+            <h4>Please click <a href='/'>here</a> to return to the Home Page.</h4>
         </Content>
         </>
     );
