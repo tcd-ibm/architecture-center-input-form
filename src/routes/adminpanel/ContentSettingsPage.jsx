4e8b1a13f9180019d4901e83b03478cd4f94dbda
Michał Bronicki <bronickm@tcd.ie>
2023-02-28 03:31:47 +0000

    Implemented dummy content settings form

--- src/routes/adminpanel/ContentSettingsPage.jsx
+++ src/routes/adminpanel/ContentSettingsPage.jsx
@@ -1,9 +1,49 @@
-import { Heading } from '@carbon/react';
+import { Form, Stack, Dropdown, Checkbox, Button } from '@carbon/react';
+
+import styles from './ContentSettingsPage.module.scss';
 
 function ContentSettingsPage() {
+    const items = [
+        {
+          id: 'everyone',
+          text: 'Everyone',
+        },
+        {
+          id: 'users',
+          text: 'Users',
+        },
+        {
+          id: 'admins',
+          text: 'Admins',
+        }
+    ];
+
     return (
         <>
-        <Heading>Not implemented</Heading>
+        <Form>
+            <Stack gap={5} className={styles.stackContainer}>
+                <Dropdown
+                    id='inline'
+                    titleText='Who can add new projects'
+                    label='Who can add new projects'
+                    initialSelectedItem={items[1]}
+                    type='inline'
+                    items={items}
+                    itemToString={(item) => (item ? item.text : '')}
+                />
+                <Checkbox 
+                    labelText='New projects have to be approved by an admin' 
+                    id='new-projects-approved-checkbox' 
+                    checked={true}
+                />
+                <Checkbox 
+                    labelText='Project edits have to be approved by an admin' 
+                    id='project-edits-approved-checkbox' 
+                    checked={true}
+                />
+                <Button>Save</Button>
+            </Stack>
+        </Form>
         </>
     );
 }
