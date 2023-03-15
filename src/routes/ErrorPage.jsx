2dc4fc51e265e0cb34576a83fe9574ff3205b733
MauvG <muaviaijazghazi@gmail.com>
2023-03-15 16:27:11 +0000

    Updated error page

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,19 +1,16 @@
-import { Content } from '@carbon/react';
+import { Content, Heading, Section} from '@carbon/react';
 import MainHeader from '@/Components/MainHeader';
-
 import styles from './ErrorPage.module.scss';
 
 function ErrorPage() {
     return (
         <>
         <MainHeader />
-        <Content className={styles.mainContainer}>
-            <h1>404 Page not found!</h1>
-            <br></br><br></br><br></br>
-            <h4>Unfortunately, the page you are looking for does not exist.</h4>
-            <br></br>
-            <h4>Please click <a href='/'>here</a> to return to the Home Page.</h4>
-        </Content>
+            <Content className={styles.mainContainer}>
+                    <Heading className={styles.notFound}>404 Page not found!</Heading>
+                    <p class='text' >Unfortunately, the page you are looking for does not exist.</p>
+                    <p class='text' >Please click <a href='/'>here</a> to return to the Home Page.</p>
+            </Content>
         </>
     );
 }
