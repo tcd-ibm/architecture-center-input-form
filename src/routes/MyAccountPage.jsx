3cc7c124401488ffd8c9969bbf0acb2f9000d034
MauvG <muaviaijazghazi@gmail.com>
2023-03-24 11:11:03 +0000

    Updated My Account Page

--- src/routes/MyAccountPage.jsx
+++ src/routes/MyAccountPage.jsx
@@ -1,14 +1,28 @@
-import { Content, Heading } from '@carbon/react';
+import { Content, Heading, Section, SideNav, SideNavItems, SideNavDivider, SideNavLink } from '@carbon/react';
 import MainHeader from '@/Components/MainHeader';
 import styles from './MyAccountPage.module.scss';
-import { useRouteError } from 'react-router-dom';
 
 function MyAccountPage() {
     return (
         <>
          <MainHeader />
-            <Content className={styles.mainContainer}>
-                <Heading>Hello</Heading>    
+            <Content>
+                
+            <SideNav className={styles.sideNav}>
+                <SideNavItems >
+                    <p1 className={styles.sideNavHeading} >My Account</p1>
+                    <SideNavDivider />
+                    <SideNavLink href='/account' >User Info</SideNavLink>
+                </SideNavItems>
+            </SideNav>
+
+            <div className={styles.mainContainer}>
+                <Heading>User Info</Heading>    
+                <div className={styles.mainText}>
+                    <p1>Email: example@gamil.com</p1>
+                </div>
+            </div>
+            
             </Content>
         </>
     );
