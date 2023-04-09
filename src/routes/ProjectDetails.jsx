af8cfb5dba91d6615a75f08ee121ccb2f01c62c1
Tom Moroney <moroneyt@tcd.ie>
2023-04-10 00:32:38 +0100

    Updated Project Details Page Design

--- src/routes/ProjectDetails.jsx
+++ src/routes/ProjectDetails.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router';
 import axios from 'axios';
 import MainHeader from '@/Components/MainHeader';
 import { Tile, Content, Grid, Column, Tag, Button } from '@carbon/react';
-import { ArrowRight, LogoGithub, DataDefinition, Document, Link} from '@carbon/icons-react';
+import { ArrowRight, LogoGithub, DataDefinition, Document, Link, Growth} from '@carbon/icons-react';
 import Asciidoctor from 'asciidoctor';
 import styles from './ProjectDetails.module.scss';
 import { useMediaQuery } from 'react-responsive';
@@ -16,11 +16,13 @@ function ProjectDetails() {
     const { projectId } = useParams();
     const [isLoading, setIsLoading] = useState(true);
     const [project, setProject] = useState();
+    const [date, setDate] = useState(new Date());
     const isOnMobile = useMediaQuery({ query: '(max-width: 671px)' });
 
     useEffect(() => {
         axios.get(`/project/${projectId}`).then(res => {
             setProject(res.data);
+            setDate((new Date(res.data.date)).toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'}));
             setIsLoading(false);
         })
         .catch(err => {
@@ -34,41 +36,32 @@ function ProjectDetails() {
             <Content className={styles.contentBox}>
                 { isLoading ? <div>Loading...</div> : 
                     <>
-                    <br></br>
-                        {/*Project heading*/}
-                        <Tile className={styles.tileContainer} style = {{paddingBottom: '20px', marginRight: '15px', marginLeft: '15px'}}>
-                            <div className={styles.divCenter}>
-                                <h2>{project.title}</h2>
-                            </div>
-                        </Tile>
-
-                    <br></br>
-
                     <Grid>
-                        <Column sm={4} md={6} lg={12} style={isOnMobile ? {} : {marginLeft: '0px', marginBottom: '20px'}} >
+                        <Column sm={4} md={2} lg={4} className={isOnMobile ? styles.mobileGrid : styles.normalGrid}>
+                        <Tile className={styles.tileContainer}>
+                            <img src={`http://localhost:5297/api/v1/project/${projectId}/image`} style={{width:'100%', maxWidth: '600px',marginBottom: '15px'}} alt='Project'
+                            onError={event => event.target.style.display = 'none'} />
+                            <h5 style={{marginBottom: '5px'}}>Description</h5>
+                            <div style={{fontSize: '100%', marginBottom: '15px'}}>{project.description}</div>
+                            <h5 style={{marginBottom: '5px'}}>Completion Date</h5>
+                            <div style={{fontSize: '100%', marginBottom: '15px'}}>{date}</div>
+                            <h5 style={{marginBottom: '5px'}}>Tags</h5>
+                            <div className='tags' style={{marginBottom: '20px'}}>
+                                {project.tags.map(tagItem => 
+                                    <Tag type={tagColors[tagItem.categoryId % 10]} title='Clear Filter' key={tagItem.tagId}>{tagItem.tagName}</Tag>
+                                )}
+                            </div>
+                            <Button href={project.link} size='md' kind='tertiary' renderIcon={Link} style={{width: '100%', maxWidth: '100%'}}>Link to Project</Button>
+                        </Tile>
+                        </Column>
+                        <Column sm={4} md={6} lg={12} style={isOnMobile ? {} : {marginLeft: '15px', marginBottom: '20px', marginRight: '0px'}} >
+                            <Tile className={styles.titleBox}>
+                                <h2>{project.title}</h2>
+                                <Tag type='outline' renderIcon={Growth} className={isOnMobile ? styles.featuredTagMobile : styles.featuredTag}>{project.visit_count} Views</Tag>
+                            </Tile>
+                            
                             <div className={styles.descriptionBody} dangerouslySetInnerHTML={{ __html: asciidoctor.convert(project.content) }} />
                             
-                        </Column>
-                        <Column sm={4} md={2} lg={4} className={isOnMobile ? styles.mobileGrid : styles.normalGrid}>
-                            <Tile className={styles.tileContainer}>
-                                <img src={`http://localhost:5297/api/v1/project/${projectId}/image`} style={{width:'100%', maxWidth: '600px',marginBottom: '15px'}} alt='Project'
-                                onError={event => event.target.style.display = 'none'} />
-                                <Button href={project.link} size='lg' kind='tertiary' renderIcon={Link} style={{width: '100%', maxWidth: '100%'}}>Link to Project</Button>
-                            </Tile>
-                            <br></br>
-                            <Tile className={styles.tileContainer}>
-                                    <h5 style={{marginBottom: '5px'}}>Description</h5>
-                                    <div style={{fontSize: '100%'}}>{project.description}</div>
-                            </Tile>
-                            <br></br>
-                            <Tile className={styles.tileContainer}>
-                                <h5 style={{marginBottom: '8px'}}>Tags</h5>
-                                <div className='tags'>
-                                    {project.tags.map(tagItem => 
-                                        <Tag type={tagColors[tagItem.categoryId % 10]} title='Clear Filter' key={tagItem.tagId}>{tagItem.tagName}</Tag>
-                                    )}
-                                </div>
-                            </Tile>
                         </Column>
                     </Grid>
                     </>
