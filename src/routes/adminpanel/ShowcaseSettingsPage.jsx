6127530d3107cdeff01e57f5fd8cc7be63fb1dd6
Daire Gielty <gieltyd@tcd.ie>
2023-04-11 14:17:33 +0100

    Added featured project removal to showcase settings

--- src/routes/adminpanel/ShowcaseSettingsPage.jsx
+++ src/routes/adminpanel/ShowcaseSettingsPage.jsx
@@ -1,11 +1,36 @@
-import { Heading, FileUploader, Form, Stack, ComboBox, Checkbox, Button, Tile } from '@carbon/react';
-import { useState } from 'react';
+import { Heading, FileUploader, Form, Stack, ComboBox, Checkbox, Button, Tile, Modal } from '@carbon/react';
+import { useState, useEffect } from 'react';
 import { storage } from './../../firebase';
 import { ref, uploadBytes } from 'firebase/storage';
+
+import axios from 'axios';
+import useAuth from '@/hooks/useAuth';
+
+import FeaturedCard from '@/Components/FeaturedCard';
 
 function ShowcaseSettingsPage() {
 
     const [headerImage, setHeaderImage] = useState(null);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [featuredProject, setFeaturedProject] = useState();
+    const { user } = useAuth();
+    const [reload, setReload] = useState(false);
+
+    useEffect(() => {
+        let flag = false;
+        axios.get('/project/featured').then(res => {
+            flag = true;
+            setFeaturedProject(res.data);
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+        if (!flag) {
+            setFeaturedProject(null);
+        }
+    }, [reload]);
+
     const url = 'https://firebasestorage.googleapis.com/v0/b/arch-center.appspot.com/o/logo.png?alt=media&token=9f7ab576-c49a-40ec-879a-152942825667';
     const uploadImage = () => {
         if (headerImage == null) return;
@@ -15,8 +40,37 @@ function ShowcaseSettingsPage() {
         });
     };
 
+    async function handleDeleteFeaturedProject() {
+        const requestConfig = { 
+            headers: { 
+                'Content-Type': 'application/json', 
+                'Accept': 'application/json', 
+                'Authorization': `Bearer ${user.accessToken}` 
+            } 
+        };
+        try {
+            console.log('trying');
+            await axios.delete('/project/featured', requestConfig);
+        } catch(error) {
+            console.log(error);
+        }
+        setModalOpen(false);
+        setReload((reload) => !reload);
+    }
+
     return (
         <>
+        <Modal open={modalOpen}
+            transactional
+            size='sm'
+            modalHeading='Are you sure you want to remove the featured project? No featured
+                          project will be visible in the showcase until you set a new one.'
+            modalLabel='Remove Featured Project'
+            primaryButtonText='Confirm'
+            secondaryButtonText='Cancel'
+            onRequestClose={() => setModalOpen(false)}
+            onRequestSubmit={() => handleDeleteFeaturedProject()}
+        />
         <Heading style={{marginBottom: '20px'}}>Showcase Settings</Heading>
         <Form>
             <Stack gap={5}>
@@ -38,25 +92,15 @@ function ShowcaseSettingsPage() {
                         }}
                     />
                 </Tile>
-                <div style = {{maxWidth: '500px'}}>
-                <ComboBox
-                    id='featured-project-dropdown'
-                    label='Featured project'
-                    titleText='Featured project'
-                    helperText='Select the project to be featured on the showcase page'
-                    items={['Project 1', 'Project 2', 'Project 3']}
-                    size = 'lg'
-                />
-                </div>
-                <Checkbox 
-                    labelText='Show project preview images on the showcase page' 
-                    id='new-projects-approved-checkbox' 
-                />
-                <Checkbox 
-                    labelText='Show featured project at top of page' 
-                    id='project-edits-approved-checkbox' 
-                />
-                <Button onClick={uploadImage} style={{marginTop: '10px', marginBottom: '50px'}}>Save</Button>
+                <Button onClick={uploadImage} style={{marginTop: '10px', marginBottom: '10px'}}>Save</Button>
+                <Heading style={{marginBottom: '10px'}}>Remove featured project</Heading>
+                { featuredProject ? <Tile style = {{maxWidth: '500px', paddingBottom: '10px', marginBottom: '5px '}}>
+                    <FeaturedCard project={featuredProject} />
+                    <Button onClick={() => setModalOpen(true)} style={{marginTop: '10px', marginBottom: '5px'}}>Remove</Button>
+                </Tile> :
+                <div style={{marginBottom: '20px'}}>
+                    There is currently no featured project. Go to 'Manage projects' to set one.
+                </div>}
             </Stack>
         </Form>
         </>
