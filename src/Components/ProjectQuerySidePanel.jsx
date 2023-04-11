2e7aba8a26a459c9fbc52723d81fcf05afb3972f
Sarah Reider-Jacks <sarahreiderjacks@yahoo.com>
2023-04-11 22:56:57 +0100

    Added responsiveness to the sidebar filters menu

--- src/Components/ProjectQuerySidePanel.jsx
+++ src/Components/ProjectQuerySidePanel.jsx
@@ -1,96 +1,164 @@
-import { useState, useRef, useImperativeHandle, forwardRef } from 'react';
-import { SideNav, Stack, Search, Accordion, AccordionItem, Checkbox, DatePicker, DatePickerInput, Tile, Toggle } from '@carbon/react';
+import {
+  useState,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+  useEffect,
+} from 'react';
+import {
+  SideNav,
+  Stack,
+  Search,
+  Accordion,
+  AccordionItem,
+  Checkbox,
+  DatePicker,
+  DatePickerInput,
+  Tile,
+  Toggle,
+} from '@carbon/react';
 import styles from './ProjectQuerySidePanel.module.scss';
 import { useMediaQuery } from 'react-responsive';
 
 function ProjectQuerySidePanel(props, ref) {
-    const { menuContent, onChange } = props;
-    const selectedTagListRef = useRef([]);
-    const [selectedTagList, setSelectedTagList] = useState([]);
-    const isOnMobile = useMediaQuery({ query: '(max-width: 760px)' });
+  const { menuContent, onChange, getExpandedState, toggleExpandedState } =
+    props;
+  const selectedTagListRef = useRef([]);
+  const [selectedTagList, setSelectedTagList] = useState([]);
+  const isOnMobile = useMediaQuery({ query: '(max-width: 760px)' });
 
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
-    const [filterDate, setFilterDate] = useState(false);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [filterDate, setFilterDate] = useState(false);
 
-    useImperativeHandle(ref, () => ({
-        get selectedTagList() {
-            return selectedTagListRef.current;
-        }
-    }), []);
+  useImperativeHandle(
+    ref,
+    () => ({
+      get selectedTagList() {
+        return selectedTagListRef.current;
+      },
+    }),
+    []
+  );
 
-    const handleFilterChange = event => {
-        const { checked, id } = event.target;
-        if (checked) {
-            const newState = [...selectedTagList, id];
-            selectedTagListRef.current = newState;
-            setSelectedTagList(newState);
-        } else {
-            const newState = selectedTagList.filter(item => item !== id);
-            selectedTagListRef.current = newState;
-            setSelectedTagList(newState);
-        }
-        onChange();
-    };
+  const handleFilterChange = (event) => {
+    const { checked, id } = event.target;
+    if (checked) {
+      const newState = [...selectedTagList, id];
+      selectedTagListRef.current = newState;
+      setSelectedTagList(newState);
+    } else {
+      const newState = selectedTagList.filter((item) => item !== id);
+      selectedTagListRef.current = newState;
+      setSelectedTagList(newState);
+    }
+    onChange();
+  };
 
-    const handleSearch = (event) => {
-        onChange();
-    };
+  const handleSearch = (event) => {
+    onChange();
+  };
 
-    return (
-        <SideNav
-            className='sideNav1'
-            isFixedNav
-            expanded={isOnMobile ? false : true}
-            isChildOfHeader={false}
-            aria-label='Search and filter'>
+  return (
+    <SideNav
+      className='sideNav1'
+      isFixedNav
+      expanded={getExpandedState()}
+      isChildOfHeader={false}
+      aria-label='Search and filter'
+    >
+      <Stack gap={5} className={styles.innerContainer}>
+        {isOnMobile ? null : (
+          <Search
+            labelText='Search'
+            placeholder='Search'
+            onChange={handleSearch}
+          />
+        )}
 
-            <Stack gap={5} className={styles.innerContainer}>
-                <Search
-                    labelText='Search'
-                    placeholder='Search'
-                    onChange={handleSearch}
-                />
-                {menuContent && <Accordion>
-                    {menuContent.map((item, index) => <AccordionItem title={item.title} key={index} open>
-                        <fieldset className='cds--fieldset'>
-                            {item.tags.map((tagItem, index) =>
-                                <Checkbox labelText={tagItem.name} id={tagItem.id} checked={selectedTagList.includes(tagItem.id)} onChange={handleFilterChange} key={index} />
-                            )}
-                        </fieldset>
-                    </AccordionItem>)}
-                </Accordion>}
-                <Tile style={{border: '1px solid gray'}}>
-                    <DatePicker style={{marginBottom: '20px'}} datePickerType='single' dateFormat='d/m/Y' value={startDate} onChange={date => setStartDate(new Date(date))}>
-                        <DatePickerInput
-                          id='date-picker-input-id-start'
-                          placeholder='dd/mm/yyyy'
-                          labelText='Start date'
-                          size='sm'
-                          style={{width: '190px'}}
-                        />
-                    </DatePicker>
-                    <DatePicker style={{marginBottom: '20px'}} datePickerType='single' dateFormat='d/m/Y' value={endDate} onChange={date => setEndDate(new Date(date))}>
-                         <DatePickerInput
-                           id='date-picker-input-id-finish'
-                           placeholder='dd/mm/yyyy'
-                           labelText='End date'
-                           size='sm'
-                           style={{width: '190px'}}
-                         />
-                    </DatePicker>
-                    <Toggle
-                        labelA='Off'
-                        labelB='On'
-                        id='toggle-1'
-                        size='sm'
-                        onToggle={event => setFilterDate(event)}
-                      />           
-                </Tile>
-            </Stack>
+        {menuContent && (
+          <Accordion>
+            {menuContent.map((item, index) => (
+              <AccordionItem title={item.title} key={index} open>
+                <fieldset className='cds--fieldset'>
+                  {item.tags.map((tagItem, index) => (
+                    <Checkbox
+                      labelText={tagItem.name}
+                      id={tagItem.id}
+                      checked={selectedTagList.includes(tagItem.id)}
+                      onChange={handleFilterChange}
+                      key={index}
+                    />
+                  ))}
+                </fieldset>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        )}
+        <Tile style={{ border: '1px solid gray' }}>
+          <DatePicker
+            style={{ marginBottom: '20px' }}
+            datePickerType='single'
+            dateFormat='d/m/Y'
+            value={startDate}
+            onChange={(date) => setStartDate(new Date(date))}
+          >
+            <DatePickerInput
+              id='date-picker-input-id-start'
+              placeholder='dd/mm/yyyy'
+              labelText='Start date'
+              size='sm'
+              style={{ width: '190px' }}
+            />
+          </DatePicker>
+          <DatePicker
+            style={{ marginBottom: '20px' }}
+            datePickerType='single'
+            dateFormat='d/m/Y'
+            value={endDate}
+            onChange={(date) => setEndDate(new Date(date))}
+          >
+            <DatePickerInput
+              id='date-picker-input-id-finish'
+              placeholder='dd/mm/yyyy'
+              labelText='End date'
+              size='sm'
+              style={{ width: '190px' }}
+            />
+          </DatePicker>
+          <Toggle
+            labelA='Off'
+            labelB='On'
+            id='toggle-1'
+            size='sm'
+            onToggle={(event) => setFilterDate(event)}
+          />
+        </Tile>
+      </Stack>
 
-        </SideNav>
-    );
+      {isOnMobile ? (
+        <div id={styles.filterOptions}>
+          <button
+            id={styles.applyFiltersBtn}
+            onClick={() => {
+              setSelectedTagList([]);
+              selectedTagListRef.current = [];
+            }}
+          >
+            Reset
+          </button>
+          <button
+            id={styles.applyFiltersBtn}
+            onClick={() => {
+              toggleExpandedState();
+            }}
+          >
+            Apply
+          </button>
+        </div>
+      ) : null}
+    </SideNav>
+  );
 }
 
 export default forwardRef(ProjectQuerySidePanel);
--- src/routes/MainPage.jsx
+++ src/routes/MainPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import { Content, Loading } from '@carbon/react';
+import { Content, Loading, Search } from '@carbon/react';
 import styles from './MainPage.module.scss';
 import { useMediaQuery } from 'react-responsive';
 
@@ -8,82 +8,145 @@ import MainHeader from '@/Components/MainHeader';
 import ProjectQuerySidePanel from '@/Components/ProjectQuerySidePanel';
 import Card from '@/Components/Card';
 import FeaturedCard from '../Components/FeaturedCard';
+import { Filter } from '@carbon/icons-react';
 
 function MainPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+  const [featuredProject, setFeaturedProject] = useState();
+  const [queryMenuContent, setQueryMenuContent] = useState([]);
+  const [sideBarExpanded, setSideBarExpanded] = useState(true);
+  const queryMenuRef = useRef();
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [projects, setProjects] = useState([]);
-    const [featuredProject, setFeaturedProject] = useState();
-    const [queryMenuContent, setQueryMenuContent] = useState([]);
-    const queryMenuRef = useRef();
+  const isOnMobile = useMediaQuery({ query: '(max-width: 760px)' });
 
-    const isOnMobile = useMediaQuery({ query: '(max-width: 760px)' });
+  useEffect(() => {
+    // sets the sidebar to be hidden by default on mobile
+    if (isOnMobile) {
+      setSideBarExpanded(false);
+    }
+  }, []);
 
-    useEffect(() => {
-        axios.get('/tags').then(res => {
-            const content = res.data.map(item => ({
-                title: item.categoryName,
-                tags: item.tags.map(tagItem => ({id: String(tagItem.tagId), name: tagItem.tagName}))
-            }));
-            setQueryMenuContent(content);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-    }, []);
+  useEffect(() => {
+    axios
+      .get('/tags')
+      .then((res) => {
+        const content = res.data.map((item) => ({
+          title: item.categoryName,
+          tags: item.tags.map((tagItem) => ({
+            id: String(tagItem.tagId),
+            name: tagItem.tagName,
+          })),
+        }));
+        setQueryMenuContent(content);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get('/projects').then(res => {
-            setProjects(res.data);
-            setIsLoading(false);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-    }, []);
+  useEffect(() => {
+    axios
+      .get('/projects')
+      .then((res) => {
+        setProjects(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get('/project/featured').then(res => {
-            setFeaturedProject(res.data);
-            console.log(res.data);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-    }, []);
+  useEffect(() => {
+    axios
+      .get('/project/featured')
+      .then((res) => {
+        setFeaturedProject(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-
-    const handleSearchAndFilterChange = () => {
-        const params = {
-            keyword: document.querySelector('input').value,
-            tags: queryMenuRef.current.selectedTagList.join(',')
-        };
-
-        axios.get('./projects', { params }).then(res => {
-            setProjects(res.data);
-            setIsLoading(false);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+  const handleSearchAndFilterChange = () => {
+    const params = {
+      keyword: document.querySelector('input').value,
+      tags: queryMenuRef.current.selectedTagList.join(','),
     };
 
-    return (
-        <>
-            <MainHeader />
-            <ProjectQuerySidePanel menuContent={queryMenuContent} ref={queryMenuRef} onChange={handleSearchAndFilterChange} />
-            <Content style={isOnMobile? {padding: '16px',  margin: 0, 'margin-top':'64px'}: {} }>
-                { isLoading ? <Loading withOverlay={false} style={{margin: 'auto', marginTop: '30px'}} /> : 
-                    <div id={styles.cardContainer}>
-                        { featuredProject && <FeaturedCard project={featuredProject} isOnMobile={isOnMobile} /> }
-                        {projects.map((projectData, index) => (
-                            <Card projectData={projectData} key={index} />
-                        ))}
-                    </div>
-                }
-            </Content>
-        </>
-    );
+    axios
+      .get('./projects', { params })
+      .then((res) => {
+        setProjects(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getExpandedState = () => {
+    return sideBarExpanded;
+  };
+
+  const toggleExpandedState = () => {
+    setSideBarExpanded(!sideBarExpanded);
+  };
+
+  return (
+    <>
+      <MainHeader />
+      <ProjectQuerySidePanel
+        menuContent={queryMenuContent}
+        ref={queryMenuRef}
+        getExpandedState={getExpandedState}
+        toggleExpandedState={toggleExpandedState}
+        onChange={handleSearchAndFilterChange}
+      />
+      <Content
+        style={
+          isOnMobile ? { padding: '16px', margin: 0, 'margin-top': '64px' } : {}
+        }
+      >
+        {isOnMobile ? (
+          <Search
+            labelText='Search'
+            placeholder='Search'
+            onChange={handleSearchAndFilterChange}
+          />
+        ) : null}
+
+        {isOnMobile ? (
+          <p
+            id={styles.filtersBtn}
+            onClick={() => {
+              toggleExpandedState();
+            }}
+          >
+            <span>Filters</span>
+            <Filter />
+          </p>
+        ) : null}
+
+        {isLoading ? (
+          <Loading
+            withOverlay={false}
+            style={{ margin: 'auto', marginTop: '30px' }}
+          />
+        ) : (
+          <div id={styles.cardContainer}>
+            {featuredProject && (
+              <FeaturedCard project={featuredProject} isOnMobile={isOnMobile} />
+            )}
+            {projects.map((projectData, index) => (
+              <Card projectData={projectData} key={index} />
+            ))}
+          </div>
+        )}
+      </Content>
+    </>
+  );
 }
 
 export default MainPage;
