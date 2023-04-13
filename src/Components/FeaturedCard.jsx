5030a4656c43280c6535118e7a08f3385966149f
Sarah Reider-Jacks <sarahreiderjacks@yahoo.com>
2023-04-13 22:24:08 +0100

    Fixed featured card size on big screens

--- src/Components/FeaturedCard.jsx
+++ src/Components/FeaturedCard.jsx
@@ -4,22 +4,51 @@ import { Star } from '@carbon/icons-react';
 
 import { CustomClickableTile, CustomLink } from './CustomCarbonNavigation';
 
-export default function FeaturedCard({project, isOnMobile}) {
+export default function FeaturedCard({ project, isOnMobile }) {
+  const tagColors = [
+    'red',
+    'magenta',
+    'purple',
+    'blue',
+    'cyan',
+    'teal',
+    'green',
+    'gray',
+    'cool-gray',
+    'warm-gray',
+    'high-contrast',
+  ];
 
-	const tagColors = ['red', 'magenta', 'purple', 'blue', 'cyan', 'teal', 'green', 'gray', 'cool-gray', 'warm-gray', 'high-contrast'];
-
-	return (
-        <>
-		<CustomClickableTile className={isOnMobile ? styles.featuredTileMobile : styles.featuredTile} href={`/details/${project.id}`} >
-			<CustomLink size='lg' className={styles.titleLink}>{project.title}</CustomLink>
-			<Tag type='red' className={isOnMobile ? styles.featuredTagMobile : styles.featuredTag} renderIcon={Star}>FEATURED PROJECT</Tag>
-			<p className={styles.description}>{project.description}</p>
-			<div className={styles.tags}>
-				{project.tags.map(tagItem => 
-					<Tag type={tagColors[tagItem.categoryId % 10]} title='Clear Filter' style={{marginLeft: '0px', marginRight: '5px'}} key={tagItem.tagId}>{tagItem.tagName}</Tag>
-				)}
-			</div>
-		</CustomClickableTile>
-		</>
-	);
+  return (
+    <>
+      <CustomClickableTile
+        className={isOnMobile ? styles.featuredTileMobile : styles.featuredTile}
+        href={`/details/${project.id}`}
+      >
+        <CustomLink size='lg' className={styles.titleLink}>
+          {project.title}
+        </CustomLink>
+        <Tag
+          type='red'
+          className={isOnMobile ? styles.featuredTagMobile : styles.featuredTag}
+          renderIcon={Star}
+        >
+          FEATURED PROJECT
+        </Tag>
+        <p className={styles.description}>{project.description}</p>
+        <div className={styles.tags}>
+          {project.tags.map((tagItem) => (
+            <Tag
+              type={tagColors[tagItem.categoryId % 10]}
+              title='Clear Filter'
+              style={{ marginLeft: '0px', marginRight: '5px' }}
+              key={tagItem.tagId}
+            >
+              {tagItem.tagName}
+            </Tag>
+          ))}
+        </div>
+      </CustomClickableTile>
+    </>
+  );
 }
